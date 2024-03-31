06fccf073b4bed04720b9e4c49b20f05abacf009
MaciejSerafin <maciek.serek.ms@gmail.com>
2024-03-31 18:02:24 +0200

    my commit

---
+++ pythonProject1/main.py
@@ -0,0 +1,42 @@
+# FileNotFound
+# with open("a_file.txt") as file:
+#     file.read()
+
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Hello")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is an error that I made up.")
+
+
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
+
+
+# KeyError
+# a_dictionary = {"key": "value"}
+# value = a_dictionary.get["non_existent_key"]
+
+#IndexError
+# fruit_list = ["apple", "banana", "pear"]
+# fruit = fruit_list[3]
+
+# TypeError
+# text = "abc"
+# print(text + 3)
+
